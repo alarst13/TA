190da2e5f25e643c031849f3b0e1620229c494ad
Mehrkish <MehrkishA8@gmail.com>
2019-05-24 22:48:25 -0700

    Initial commit

---
+++ app/src/main/java/com/example/ta/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.ta;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button submit_btn;
+    EditText phone_number_edt;
+    public static final String MyPREFERENCES = "MyPrefs";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffb2b2")));
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.red));
+        }
+
+        submit_btn = findViewById(R.id.phone_number_btn_main_activity);
+        phone_number_edt = findViewById(R.id.phone_number_edt_main_activity);
+
+        if (PreferenceData.getUserLoggedInStatus(getApplicationContext()) == true) {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            intent.putExtra("number", PreferenceData.getLoggedInPhoneNumberUser(this));
+            startActivity(intent);
+        }
+
+        submit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber;
+                phoneNumber = phone_number_edt.getText().toString();
+                PreferenceData.setUserLoggedInStatus(getApplicationContext(), true);
+                PreferenceData.setLoggedInUserPhoneNumber(getApplicationContext(), phoneNumber);
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("number", phoneNumber);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/ta/SecondActivity.java
@@ -0,0 +1,49 @@
+package com.example.ta;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    TextView phone_number_txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffb2b2")));
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.red));
+        }
+
+        phone_number_txt = findViewById(R.id.phone_number_txt_second_activity);
+
+        String phoneNumber = getIntent().getExtras().getString("number");
+        phone_number_txt.setText(phoneNumber);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
